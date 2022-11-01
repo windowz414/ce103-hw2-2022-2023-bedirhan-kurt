b696321b3a2440c80052735969f293c06210ba99
Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>
2022-11-01 20:52:55 +0300

    doxygen/doxy_run(sh): Fix if-else statement
    Signed-off-by: Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>

--- doxygen/doxy_run.sh
+++ doxygen/doxy_run.sh
@@ -4,9 +4,9 @@ export SOURCE_DIR=../ce103-hw2-lib
 export DOC_IMG_PATH_UML=../doxygen/Resources
 export DOC_IMG_PATH=../doxygen/Resources
 # export STRIP_PATH="C:xx"
-if [ ! -f plantuml.jar ] {
+if [ ! -f plantuml.jar ]; then
   curl -L -o plantuml.jar "https://github.com/plantuml/plantuml/releases/download/v1.2021.14/plantuml-1.2021.14.jar" || wget -O platuml.jar "https://github.com/plantuml/plantuml/releases/download/v1.2021.14/plantuml-1.2021.14.jar"
-}
+fi
 java -jar "plantuml.jar" -v -o "$DOC_IMG_PATH_UML" "$SOURCE_DIR/**.(c|cpp|doc|h|puml)"
 $DOXYGEN_EXE Doxyfile
 pause
