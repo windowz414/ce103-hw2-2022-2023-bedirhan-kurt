2532a826e55ad510b39e26e51fe202afb1fbd065
Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>
2022-11-03 19:26:10 +0300

    doxy_run: Fix downloading of PlantUML Java file and Doxygen exec
    * WHY DON'T YOU FOLLOW REDIRECTS?
    
    * WHY WOULD YOU SET A VARIABLE IF YOU WON'T USE IT???
    
    Signed-off-by: Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>

--- doxygen/doxy_run.bat
+++ doxygen/doxy_run.bat
@@ -4,7 +4,7 @@ SET SOURCE_DIR=..\ce103-hw2-lib
 SET DOC_IMG_PATH_UML=..\doxygen\Resources
 SET DOC_IMG_PATH=../doxygen/Resources
 :: SET STRIP_PATH="C:xx"
-IF NOT EXIST plantuml.jar ( curl -o plantuml.jar "https://github.com/plantuml/plantuml/releases/download/v1.2021.14/plantuml-1.2021.14.jar" )
+IF NOT EXIST plantuml.jar ( curl -L -o plantuml.jar "https://github.com/plantuml/plantuml/releases/download/v1.2021.14/plantuml-1.2021.14.jar" )
 java -jar "plantuml.jar" -v -o "%DOC_IMG_PATH_UML%" "%SOURCE_DIR%/**.(c|cpp|doc|h|puml)"
-doxygen Doxyfile
+%DOXYGEN_EXE% Doxyfile
 pause
