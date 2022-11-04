425c1ce808d97263c0d001c665b9234703f6f714
Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>
2022-11-04 20:46:10 +0300

    doxygen/doxy_run(sh): Optimize code
    Signed-off-by: Bedirhan KURT <bedirhan.kurt.trjp@gmail.com>

--- doxygen/doxy_run.sh
+++ doxygen/doxy_run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 export GRAPHVIZ_DOT="$(which dot)"
 export DOXYGEN_EXE="$(which doxygen)"
 export SOURCE_DIR=../ce103-hw2-lib
@@ -10,4 +11,4 @@ fi
 java -jar "plantuml.jar" -v -o "$DOC_IMG_PATH_UML" "$SOURCE_DIR/**.(c|cpp|doc|h|puml)"
 $DOXYGEN_EXE Doxyfile
 echo -n "Press any key to continue ..."
-read
+read -sn1 < /dev/tty
